a810236633aeabc7d1039e6a751495ec41fcfe3e
lucas <luucasfarias21@gmail.com>
2018-11-05 20:59:42 -0300

    new component release register form

---
+++ src/app/release-register/release-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-release-register',
+  templateUrl: './release-register.component.html',
+  styleUrls: ['./release-register.component.css']
+})
+export class ReleaseRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
