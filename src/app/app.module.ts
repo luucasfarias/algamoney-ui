4fa3b35039e43e3aebe24b966051540661d16b9e
lucas <luucasfarias21@gmail.com>
2018-11-05 20:58:48 -0300

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { ButtonModule } from 'primeng/components/button/button';
 import { TableModule } from 'primeng/components/table/table';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
 import { TooltipModule } from 'primeng/components/tooltip/tooltip';
+import { InputTextareaModule } from 'primeng/components/inputtextarea/inputtextarea';
+
 import { ReleaseSearchComponent } from './release-search/release-search.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReleasePeopleComponent } from './release-people/release-people.component';
-
+import { ReleaseRegisterComponent } from './release-register/release-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReleaseSearchComponent,
     NavbarComponent,
-    ReleasePeopleComponent
+    ReleasePeopleComponent,
+    ReleaseRegisterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ButtonModule,
-    TableModule,
     InputTextModule,
+    InputTextareaModule,
+    TableModule,
     TooltipModule
   ],
   providers: [],
