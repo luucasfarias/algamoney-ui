2bf5e5fe258873671580a6d8eef10dd85556909c
lucas <luucasfarias21@gmail.com>
2018-11-05 21:00:01 -0300

    fix pipe date and currency mask

--- src/app/release-search/release-search.component.ts
+++ src/app/release-search/release-search.component.ts
@@ -8,20 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class ReleaseSearchComponent implements OnInit {
 
   releases = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
-    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-      dataPagamento: '09/06/2017', valor: 80000, pessoa: 'Atacado Brasil' },
-    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
-      dataPagamento: null, valor: 14312, pessoa: 'Ministério da Fazenda' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
-      dataPagamento: '30/05/2017', valor: 800, pessoa: 'Escola Abelha Rainha' },
-    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
-      dataPagamento: null, valor: 55000, pessoa: 'Sebastião Souza' },
-    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
-      dataPagamento: '09/07/2017', valor: 1750, pessoa: 'Casa Nova Imóveis' },
-    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
-      dataPagamento: null, valor: 180, pessoa: 'Academia Top' }
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2018, 5, 20),
+      dataPagamento: new Date(2018, 6, 20), valor: 4.55, pessoa: 'Padaria do José' },
+    { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: new Date(2018, 2, 10),
+      dataPagamento: new Date(2018, 3, 20), valor: 80000, pessoa: 'Atacado Brasil' },
+    { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: new Date(2018, 3, 25),
+      dataPagamento: new Date(2018, 4, 20), valor: 14312, pessoa: 'Ministério da Fazenda' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: new Date(2018, 9, 1),
+      dataPagamento: new Date(2018, 10, 20), valor: 800, pessoa: 'Escola Abelha Rainha' },
+    { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: new Date(2018, 5, 20),
+      dataPagamento: new Date(2018, 6, 20), valor: 55000, pessoa: 'Sebastião Souza' },
+    { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2018, 12, 10),
+      dataPagamento: new Date(2019, 1, 10), valor: 1750, pessoa: 'Casa Nova Imóveis' },
+    { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: new Date(2018, 5, 20),
+      dataPagamento: new Date(2018, 6, 20), valor: 180, pessoa: 'Academia Top' }
   ];
 
   constructor() { }
