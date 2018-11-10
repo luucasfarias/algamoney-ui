fb28bc7d99753c59c8915492ead57846b507c414
lucas <luucasfarias21@gmail.com>
2018-11-10 11:33:38 -0300

    component register people

---
+++ src/app/register-people/register-people.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-people',
+  templateUrl: './register-people.component.html',
+  styleUrls: ['./register-people.component.css']
+})
+export class RegisterPeopleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
