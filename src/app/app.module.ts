7f6d0f3379e0f63d39579ef2aee02c2f25946087
lucas <luucasfarias21@gmail.com>
2018-11-15 21:12:44 -0300

    new components and module core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
-import { NavbarComponent } from './navbar/navbar.component';
-
+import { HttpModule } from '@angular/http';
 import { ReleaseModule } from './release/release.module';
 import { PeopleModule } from './people/people.module';
 import { SharedModule } from './shared/shared.module';
+import { CormoduleModule } from './cormodule/cormodule.module';
+import { ReleaseService } from './release/release.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpModule,
 
     ReleaseModule,
     PeopleModule,
-    SharedModule
+    SharedModule,
+    CormoduleModule
   ],
-  providers: [],
+  providers: [ReleaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
